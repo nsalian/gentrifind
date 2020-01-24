98dea75913416031f8789a5d9e126d54822f3517
nidhi <nidhixsalian@gmail.com>
2020-01-24 18:08:51 -0500

    Added Description Activity

--- app/src/main/java/com/example/jjmentorship/MainActivity.java
+++ app/src/main/java/com/example/jjmentorship/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -70,9 +72,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button descriptionButton = findViewById(R.id.descriptionButton);
+        descriptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transition = new Intent(v.getContext(), Description.class);
+                startActivity(transition);
+            }
+        });
+
+
         finalPrice = findViewById(R.id.finalPrice);
         averagePrice = findViewById(R.id.averagePrice);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            averagePrice.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
+        }
         modifiedPrice = findViewById(R.id.modifiedPrice);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            modifiedPrice.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
+        }
         medianListingTextView = findViewById(R.id.medianListingText);
 
     }
